21b4bc42d85b704393e69d4f8b0bd8561ad3c752
sisso <sisso@sisso.com.br>
2019-09-29 12:54:58 +0200

    Animating sectors

--- rust/space-console/examples/draw_map.rs
+++ rust/space-console/examples/draw_map.rs
@@ -24,6 +24,7 @@ use tui::Terminal;
 use space_domain::utils::V2;
 use crate::util::event::{Config, Event, Events};
 
+#[derive(Debug, Copy, Clone)]
 enum ObjKind {
     JUMP,
     SHIP,
@@ -31,16 +32,19 @@ enum ObjKind {
     ASTEROID,
 }
 
+#[derive(Debug, Clone)]
 struct Obj {
     kind: ObjKind,
     pos: V2,
 }
 
+#[derive(Debug, Clone)]
 struct Sector {
     label: String,
     objects: Vec<Obj>,
 }
 
+#[derive(Debug, Clone)]
 struct State {
     sectors: Vec<Sector>
 }
@@ -49,7 +53,7 @@ fn main() {
     // Terminal initialization
     let stdout = io::stdout().into_raw_mode().unwrap();
 //    let stdout = MouseTerminal::from(stdout);
-//    let stdout = AlternateScreen::from(stdout);
+    let stdout = AlternateScreen::from(stdout);
     let backend = TermionBackend::new(stdout);
     let mut terminal = Terminal::new(backend).unwrap();
     terminal.hide_cursor().unwrap();
@@ -66,22 +70,50 @@ fn main() {
         sectors: vec![
             Sector {
                 label: "Sector 0".to_string(),
-                objects: vec![]
+                objects: vec![
+                    Obj {
+                        kind: ObjKind::JUMP,
+                        pos: V2::new(7.0, 5.0)
+                    },
+                    Obj {
+                        kind: ObjKind::ASTEROID,
+                        pos: V2::new(-3.0, -5.0)
+                    },
+                ]
             },
             Sector {
-                label: "Sector 0".to_string(),
-                objects: vec![]
+                label: "Sector 1".to_string(),
+                objects: vec![
+                    Obj {
+                        kind: ObjKind::JUMP,
+                        pos: V2::new(-4.0, 2.0)
+                    },
+                    Obj {
+                        kind: ObjKind::STATION,
+                        pos: V2::new(0.0, -5.0)
+                    },
+                    Obj {
+                        kind: ObjKind::SHIP,
+                        pos: V2::new(0.0, 0.0)
+                    },
+                ]
             },
         ]
     };
 
-
     debugf!("start");
+
+    fn color_from_kind(kind: ObjKind) -> Color {
+        match kind {
+            ObjKind::SHIP => Color::Blue,
+            ObjKind::STATION => Color::Green,
+            ObjKind::ASTEROID => Color::Gray,
+            ObjKind::JUMP => Color::Red,
+        }
+    }
 
     loop {
         terminal.draw(|mut f| {
-            let window_title = "Sector";
-
             let percentage_per_sector = (100.0 / state.sectors.len() as f32) as u16;
 
             let mut layout_constraint = vec![];
@@ -94,33 +126,22 @@ fn main() {
                 .constraints(layout_constraint)
                 .split(f.size());
 
-            Canvas::default()
-                .block(Block::default().borders(Borders::ALL).title(window_title))
-                .paint(|ctx| {
-                    let points: Points = Points {
-                        coords: &[(0.0, 0.0)],
-                        color: Color::Red
-                    };
-                    ctx.draw(&points);
-//                    ctx.print(app.x, -app.y, "You are here", Color::Yellow);
-                })
-                .x_bounds([-180.0, 180.0])
-                .y_bounds([-90.0, 90.0])
-                .render(&mut f, chunks[0]);
-
-            Canvas::default()
-                .block(Block::default().borders(Borders::ALL).title(window_title))
-                .paint(|ctx| {
-                    let points: Points = Points {
-                        coords: &[(0.0, 0.0)],
-                        color: Color::Red
-                    };
-                    ctx.draw(&points);
-//                    ctx.print(app.x, -app.y, "You are here", Color::Yellow);
-                })
-                .x_bounds([-180.0, 180.0])
-                .y_bounds([-90.0, 90.0])
-                .render(&mut f, chunks[1]);
+            for (index, sector) in state.sectors.iter().enumerate() {
+                Canvas::default()
+                    .block(Block::default().borders(Borders::ALL).title(sector.label.as_str()))
+                    .paint(|ctx| {
+                        for obj in &sector.objects {
+                            let points: Points = Points {
+                                coords: &[(obj.pos.x as f64, obj.pos.y as f64)],
+                                color: color_from_kind(obj.kind)
+                            };
+                            ctx.draw(&points);
+                        }
+                    })
+                    .x_bounds([-10.0, 10.0])
+                    .y_bounds([-10.0, 10.0])
+                    .render(&mut f, chunks[index]);
+            }
         }).unwrap();
 
         match events.next().unwrap() {
@@ -133,6 +154,16 @@ fn main() {
                 }
             },
             Event::Tick => {
+                for s in &mut state.sectors {
+                    for o in &mut s.objects {
+                        match o.kind {
+                            ObjKind::SHIP => {
+                                o.pos = o.pos.add(&V2::new(0.1, 0.0));
+                            },
+                            _ => {}
+                        }
+                    }
+                }
             }
         }
     }
