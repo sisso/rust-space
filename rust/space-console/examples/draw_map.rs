5bfc97ffd3cd9b3fa9f928795fbb4c76302d0a85
sisso <sisso@sisso.com.br>
2019-09-29 12:39:38 +0200

    Working in display sectors

--- rust/space-console/examples/draw_map.rs
+++ rust/space-console/examples/draw_map.rs
@@ -1,6 +1,9 @@
 #[allow(dead_code)]
 extern crate tui;
 extern crate termion;
+extern crate space_domain;
+#[macro_use]
+extern crate space_macros;
 
 mod util;
 
@@ -18,65 +21,38 @@ use tui::widgets::canvas::{Canvas, Map, MapResolution, Rectangle, Points};
 use tui::widgets::{Block, Borders, Widget};
 use tui::Terminal;
 
+use space_domain::utils::V2;
 use crate::util::event::{Config, Event, Events};
 
-struct App {
-    x: f64,
-    y: f64,
-    ball: Rect,
-    playground: Rect,
-    vx: u16,
-    vy: u16,
-    dir_x: bool,
-    dir_y: bool,
+enum ObjKind {
+    JUMP,
+    SHIP,
+    STATION,
+    ASTEROID,
 }
 
-impl App {
-    fn new() -> App {
-        App {
-            x: 0.0,
-            y: 0.0,
-            ball: Rect::new(10, 30, 10, 10),
-            playground: Rect::new(10, 10, 100, 100),
-            vx: 1,
-            vy: 1,
-            dir_x: true,
-            dir_y: true,
-        }
-    }
-
-    fn update(&mut self) {
-        if self.ball.left() < self.playground.left() || self.ball.right() > self.playground.right()
-        {
-            self.dir_x = !self.dir_x;
-        }
-        if self.ball.top() < self.playground.top() || self.ball.bottom() > self.playground.bottom()
-        {
-            self.dir_y = !self.dir_y;
-        }
-
-        if self.dir_x {
-            self.ball.x += self.vx;
-        } else {
-            self.ball.x -= self.vx;
-        }
-
-        if self.dir_y {
-            self.ball.y += self.vy;
-        } else {
-            self.ball.y -= self.vy
-        }
-    }
+struct Obj {
+    kind: ObjKind,
+    pos: V2,
 }
 
-fn main() -> Result<(), std::io::Error> {
+struct Sector {
+    label: String,
+    objects: Vec<Obj>,
+}
+
+struct State {
+    sectors: Vec<Sector>
+}
+
+fn main() {
     // Terminal initialization
-    let stdout = io::stdout().into_raw_mode()?;
-    let stdout = MouseTerminal::from(stdout);
-    let stdout = AlternateScreen::from(stdout);
+    let stdout = io::stdout().into_raw_mode().unwrap();
+//    let stdout = MouseTerminal::from(stdout);
+//    let stdout = AlternateScreen::from(stdout);
     let backend = TermionBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
-    terminal.hide_cursor()?;
+    let mut terminal = Terminal::new(backend).unwrap();
+    terminal.hide_cursor().unwrap();
 
     // Setup event handlers
     let config = Config {
@@ -86,12 +62,37 @@ fn main() -> Result<(), std::io::Error> {
     let events = Events::with_config(config);
 
     // App
-    let mut app = App::new();
+    let mut state = State {
+        sectors: vec![
+            Sector {
+                label: "Sector 0".to_string(),
+                objects: vec![]
+            },
+            Sector {
+                label: "Sector 0".to_string(),
+                objects: vec![]
+            },
+        ]
+    };
+
+
+    debugf!("start");
 
     loop {
         terminal.draw(|mut f| {
-            let rect = f.size();
             let window_title = "Sector";
+
+            let percentage_per_sector = (100.0 / state.sectors.len() as f32) as u16;
+
+            let mut layout_constraint = vec![];
+            layout_constraint.resize(state.sectors.len(), Constraint::Percentage(percentage_per_sector));
+
+            debugf!("constraints {:?}", layout_constraint);
+
+            let chunks = Layout::default()
+                .direction(Direction::Horizontal)
+                .constraints(layout_constraint)
+                .split(f.size());
 
             Canvas::default()
                 .block(Block::default().borders(Borders::ALL).title(window_title))
@@ -105,34 +106,36 @@ fn main() -> Result<(), std::io::Error> {
                 })
                 .x_bounds([-180.0, 180.0])
                 .y_bounds([-90.0, 90.0])
-                .render(&mut f, rect);
+                .render(&mut f, chunks[0]);
+
+            Canvas::default()
+                .block(Block::default().borders(Borders::ALL).title(window_title))
+                .paint(|ctx| {
+                    let points: Points = Points {
+                        coords: &[(0.0, 0.0)],
+                        color: Color::Red
+                    };
+                    ctx.draw(&points);
+//                    ctx.print(app.x, -app.y, "You are here", Color::Yellow);
+                })
+                .x_bounds([-180.0, 180.0])
+                .y_bounds([-90.0, 90.0])
+                .render(&mut f, chunks[1]);
         }).unwrap();
 
         match events.next().unwrap() {
             Event::Input(input) => match input {
-                Key::Char('q') => {
+                Key::Char('q') | Key::Ctrl('c') => {
                     break;
+                },
+                other => {
+                    debugf!("receive key {:?}", other);
                 }
-                Key::Down => {
-                    app.y += 1.0;
-                }
-                Key::Up => {
-                    app.y -= 1.0;
-                }
-                Key::Right => {
-                    app.x += 1.0;
-                }
-                Key::Left => {
-                    app.x -= 1.0;
-                }
-
-                _ => {}
             },
             Event::Tick => {
-                app.update();
             }
         }
     }
 
-    Ok(())
+    debugf!("complete");
 }
--- rust/space-macros/src/lib.rs
+++ rust/space-macros/src/lib.rs
@@ -73,3 +73,34 @@ macro_rules! warn {
                  msg=format!($fmt, $($arg)*).as_str());
     );
 }
+
+// ----------------------------------------------------
+
+#[macro_export]
+macro_rules! debugf {
+    () => (debugf!(""));
+    ($fmt:expr) => (match ::std::fs::OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open("/tmp/debug.log") {
+            Ok(mut file) => {
+                use std::io::Write;
+                file.write_all(format!("{}\n", $fmt).as_bytes()).ok();
+            }
+            Err(_) => {
+                panic!("failed to open log file")
+            },
+        });
+    ($fmt:expr, $($arg:tt)*) => (match ::std::fs::OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open("/tmp/debug.log") {
+            Ok(mut file) => {
+                use std::io::Write;
+                file.write_all(format!(concat!($fmt, "\n"), $($arg)*).as_bytes()).ok();
+            }
+            Err(_) => {
+                panic!("failed to open log file")
+            },
+        });
+}
