ecc16f1abf4b14cf7d365948d4366e97f107b8ae
sisso <sisso@sisso.com.br>
2020-03-01 15:38:21 +0100

    fix full cargo test for factory

--- rust/space-domain/src/game/factory.rs
+++ rust/space-domain/src/game/factory.rs
@@ -115,12 +115,18 @@ mod test {
     const ORE_ID: WareId = WareId(0);
     const ENERGY_ID: WareId = WareId(1);
     const PLATE_ID: WareId = WareId(2);
-    
+
+    const REQUIRE_ORE: f32 = 1.0;
+    const REQUIRE_ENERGY: f32 = 10.0;
+    const PRODUCTION_TIME: f32 = 5.0;
+    const TOTAL_CARGO: f32 = 20.0;
+    const PRODUCED_PLATE: f32 = 1.0;
+
     fn get_production() -> Production {
         Production {
-            input: vec![WareAmount(ORE_ID, 1.0), WareAmount(ENERGY_ID, 10.0)],
-            output: vec![WareAmount(PLATE_ID, 1.0)],
-            time: DeltaTime(5.0),
+            input: vec![WareAmount(ORE_ID, REQUIRE_ORE), WareAmount(ENERGY_ID, REQUIRE_ENERGY)],
+            output: vec![WareAmount(PLATE_ID, PRODUCED_PLATE)],
+            time: DeltaTime(PRODUCTION_TIME),
         }
     }
 
@@ -131,27 +137,29 @@ mod test {
 
     #[test]
     fn test_factory_system_should_not_start_production_without_both_enough_cargo() {
-        run_factory(2.0, 0.0, 3.0, None, None, 2.0, 0.0, 0.0);
+        let ore_amount = REQUIRE_ORE - 0.5;
+        run_factory(ore_amount, 0.0, 3.0, None, None, ore_amount, 0.0, 0.0);
     }
 
     #[test]
     fn test_factory_system_should_start_production_with_both_cargo() {
-        run_factory(2.0, 10.0, 3.0, None, Some(8.0), 0.0, 1.0, 0.0);
+        let current_time = 3.0;
+        run_factory(REQUIRE_ORE, REQUIRE_ENERGY, current_time, None, Some(current_time + PRODUCTION_TIME as f64), 0.0, 1.0, 0.0);
     }
 
     #[test]
     fn test_factory_system_should_keep_producing() {
-        run_factory(2.0, 10.0, 3.0, Some(8.0), Some(8.0), 2.0, 10.0, 0.0);
+        run_factory(REQUIRE_ORE, REQUIRE_ENERGY, 3.0, Some(8.0), Some(8.0), REQUIRE_ORE, REQUIRE_ENERGY, 0.0);
     }
 
     #[test]
     fn test_factory_system_should_produce() {
-       run_factory(0.0, 0.0, 9.0, Some(8.0), None, 0.0, 0.0, 1.0);
+       run_factory(0.0, 0.0, 9.0, Some(8.0), None, 0.0, 0.0, PRODUCED_PLATE);
     }
 
     #[test]
     fn test_factory_system_should_not_complete_production_if_cargo_is_full() {
-        unimplemented!()
+        run_factory(TOTAL_CARGO, 0.0, 9.0, Some(8.0), Some(8.0), 0.0, 0.0, 0.0);
     }
 
     fn run_factory(ore: f32,
@@ -163,7 +171,7 @@ mod test {
                    expected_energy: f32,
                    expected_plates: f32) {
        let (world, entity) = test_system(FactorySystem, move |world| {
-           let mut cargo = Cargo::new(100.0);
+           let mut cargo = Cargo::new(TOTAL_CARGO);
            cargo.add(ORE_ID, ore).unwrap();
            cargo.add(ENERGY_ID, energy).unwrap();
 
