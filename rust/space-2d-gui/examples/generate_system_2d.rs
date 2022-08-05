a22b7a0739043ac90db71628486e4321a9852f21
sisso <sissobr@mail.com>
2022-08-04 21:22:01 -0300

    improvements on 2d view

--- rust/space-2d-gui/examples/generate_system_2d.rs
+++ rust/space-2d-gui/examples/generate_system_2d.rs
@@ -1,12 +1,13 @@
 use commons::math::{P2, V2};
 use ggez::conf::WindowMode;
-use ggez::event::{self, EventHandler, KeyMods, MouseButton};
+use ggez::event::{self, Axis, Button, ErrorOrigin, EventHandler, GamepadId, KeyMods, MouseButton};
 use ggez::graphics::{self, Color, DrawMode, Rect};
+use ggez::winit::event::VirtualKeyCode;
 use ggez::{Context, ContextBuilder, GameError, GameResult};
 use rand::prelude::StdRng;
 use space_2d_gui::system_generator::*;
 
-const BODY_SIZE: f32 = 0.5;
+const BODY_SIZE: f32 = 0.25;
 
 struct Viewer {
     screen_width: i32,
@@ -14,6 +15,7 @@ struct Viewer {
     system: System,
     seed: u64,
     center: V2,
+    target: P2,
     zoom: f32,
     selected: Option<SelectedObject>,
 }
@@ -49,6 +51,7 @@ fn main() {
         system,
         seed: 0,
         center: origin,
+        target: P2::new(0.0, 0.0),
         zoom: 1.0,
         selected: None,
     };
@@ -63,6 +66,35 @@ fn load_system(seed: u64) -> System {
     );
     let system = new_system(&cfg, &mut rng);
     system
+}
+
+impl Viewer {
+    fn screen_to_world(&self, pos: P2) -> P2 {
+        (pos - self.center) / self.zoom
+    }
+
+    fn world_to_screen(&self, p2: P2) -> P2 {
+        p2 * self.zoom + self.center
+    }
+
+    fn compute_local_pos(&self, index: usize, total_time: f32) -> P2 {
+        let bodies = &self.system.bodies;
+
+        if index == 0 {
+            P2::new(0.0, 0.0)
+        } else {
+            let relative = commons::math::rotate_vector_by_angle(
+                P2::new(1.0, 0.0) * bodies[index].distance * 50.0,
+                bodies[index].angle + bodies[index].speed * total_time * 0.25,
+            );
+            self.compute_local_pos(bodies[index].parent, total_time) + relative.coords
+        }
+    }
+    fn compute_pos(&self, index: usize, total_time: f32) -> P2 {
+        let value = self.compute_local_pos(index, total_time);
+
+        self.world_to_screen(value)
+    }
 }
 
 impl EventHandler<GameError> for Viewer {
@@ -95,46 +127,62 @@ impl EventHandler<GameError> for Viewer {
         }
     }
 
+    fn mouse_button_down_event(&mut self, _ctx: &mut Context, button: MouseButton, x: f32, y: f32) {
+        if button == MouseButton::Right {
+            let v2screen = P2::new(x, y);
+            self.target = self.screen_to_world(v2screen);
+        }
+    }
+
     fn mouse_button_up_event(&mut self, ctx: &mut Context, button: MouseButton, x: f32, y: f32) {
         if button == MouseButton::Left {
             let total_time = ggez::timer::time_since_start(ctx).as_secs_f32();
             let mouse_pos = P2::new(x, y);
+            let mut min_index = None;
+            let mut min_dist = None;
 
             for (i, b) in self.system.bodies.iter().enumerate() {
-                let pos =
-                    compute_pos(&self.system.bodies, i, total_time) * self.zoom + &self.center;
+                let pos = self.compute_pos(i, total_time);
                 let radius = BODY_SIZE * b.size * self.zoom;
 
-                if (pos - &mouse_pos).magnitude() < radius {
-                    let text = {
-                        match &b.desc {
-                            BodyDesc::Planet {
-                                atmosphere,
-                                gravity,
-                                biome,
-                                ocean,
-                                ..
-                            } => format!(
-                                "planet {}\nsize: {}\natmosphere: {}\ngravity: {}\nbiome: {}\nocean: {}",
-                                i, b.size, atmosphere, gravity, biome, ocean
-                            ),
-                            BodyDesc::Star { .. } => "star".to_string(),
-                            _ => "unknown".to_string(),
-                        }
-                    };
-                    let selected = SelectedObject {
-                        index: 0,
-                        text: ggez::graphics::Text::new(text),
-                    };
-                    self.selected = Some(selected);
+                let dist = (pos - &mouse_pos).magnitude();
+                if min_dist.is_none() || dist < min_dist.unwrap() {
+                    min_dist = Some(dist);
+                    min_index = Some(i);
                 }
+            }
+
+            if let Some(i) = &min_index {
+                let b = &self.system.bodies[*i];
+
+                let text = {
+                    match &b.desc {
+                        BodyDesc::Planet {
+                            atmosphere,
+                            gravity,
+                            biome,
+                            ocean,
+                            ..
+                        } => format!(
+                            "planet {}\nsize: {}\natmosphere: {}\ngravity: {}\nbiome: {}\nocean: {}",
+                            i, b.size, atmosphere, gravity, biome, ocean
+                        ),
+                        BodyDesc::Star { .. } => "star".to_string(),
+                        _ => "unknown".to_string(),
+                    }
+                };
+                let selected = SelectedObject {
+                    index: *i,
+                    text: ggez::graphics::Text::new(text),
+                };
+                self.selected = Some(selected);
             }
         } else if button == MouseButton::Right {
             self.selected = None;
         }
     }
 
-    fn mouse_motion_event(&mut self, ctx: &mut Context, _x: f32, _y: f32, dx: f32, dy: f32) {
+    fn mouse_motion_event(&mut self, ctx: &mut Context, x: f32, y: f32, dx: f32, dy: f32) {
         if ggez::input::mouse::button_pressed(ctx, MouseButton::Left) {
             self.center.x += dx;
             self.center.y += dy;
@@ -151,8 +199,7 @@ impl EventHandler<GameError> for Viewer {
 
         for (i, b) in self.system.bodies.iter().enumerate() {
             if i == 0 {
-                let pos =
-                    compute_pos(&self.system.bodies, i, total_time) * self.zoom + &self.center;
+                let pos = self.compute_pos(i, total_time);
 
                 let mesh = ggez::graphics::Mesh::new_circle(
                     ctx,
@@ -164,8 +211,7 @@ impl EventHandler<GameError> for Viewer {
                 )?;
                 graphics::draw(ctx, &mesh, graphics::DrawParam::default())?;
             } else {
-                let pos =
-                    compute_pos(&self.system.bodies, i, total_time) * self.zoom + &self.center;
+                let pos = self.compute_pos(i, total_time);
 
                 // draw body
                 let mesh = ggez::graphics::Mesh::new_circle(
@@ -179,8 +225,7 @@ impl EventHandler<GameError> for Viewer {
                 graphics::draw(ctx, &mesh, graphics::DrawParam::default())?;
 
                 // draw orbit
-                let parent_pos = compute_pos(&self.system.bodies, b.parent, total_time) * self.zoom
-                    + &self.center;
+                let parent_pos = self.compute_pos(b.parent, total_time);
                 let dist: f32 = (pos - parent_pos.clone()).magnitude();
 
                 let mesh = ggez::graphics::Mesh::new_circle(
@@ -222,18 +267,17 @@ impl EventHandler<GameError> for Viewer {
             _ => {}
         }
 
-        graphics::present(ctx)
-    }
-}
+        // draw target
+        let mesh = ggez::graphics::Mesh::new_circle(
+            ctx,
+            DrawMode::stroke(1.0),
+            self.target * self.zoom + self.center,
+            1.0,
+            1.0,
+            Color::YELLOW,
+        )?;
+        graphics::draw(ctx, &mesh, graphics::DrawParam::default())?;
 
-fn compute_pos(bodies: &Vec<SpaceBody>, index: usize, total_time: f32) -> P2 {
-    if index == 0 {
-        P2::new(0.0, 0.0)
-    } else {
-        let relative = commons::math::rotate_vector_by_angle(
-            P2::new(1.0, 0.0) * bodies[index].distance * 50.0,
-            bodies[index].angle + bodies[index].speed * total_time * 0.5,
-        );
-        compute_pos(bodies, bodies[index].parent, total_time) + relative.coords
+        graphics::present(ctx)
     }
 }
