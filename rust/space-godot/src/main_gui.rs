fb2d14a037d5b8336b282ee8cfa6d28cb3baf4d0
sisso <349345+sisso@users.noreply.github.com>
2023-03-22 17:45:09 -0300

    updating fleets ui

--- rust/space-godot/src/main_gui.rs
+++ rust/space-godot/src/main_gui.rs
@@ -21,11 +21,7 @@ impl MainGui {
             .unwrap()
             .cast::<BoxContainer>();
 
-        for c in container.get_children(true).iter_shared() {
-            let mut n = c.cast::<Node>();
-            container.remove_child(n.share());
-            n.queue_free();
-        }
+        Self::clear(&mut container);
 
         let mut h1 = HBoxContainer::new_alloc();
         let mut b1 = Button::new_alloc();
@@ -45,6 +41,30 @@ impl MainGui {
         h1.add_child(b1.upcast(), false, InternalMode::INTERNAL_MODE_DISABLED);
         container.add_child(h1.upcast(), false, InternalMode::INTERNAL_MODE_DISABLED);
     }
+
+    pub fn show_fleets(&self) {
+        let mut container = self
+            .base
+            .find_child("FleetsVBoxContainer".into(), true, true)
+            .unwrap()
+            .cast::<BoxContainer>();
+
+        Self::clear(&mut container);
+
+        for i in 0..4 {
+            let mut b1 = Button::new_alloc();
+            b1.set_text(format!("Fleet {i}").into());
+            container.add_child(b1.upcast(), false, InternalMode::INTERNAL_MODE_DISABLED);
+        }
+    }
+
+    fn clear(container: &mut Gd<BoxContainer>) {
+        for c in container.get_children(true).iter_shared() {
+            let mut n = c.cast::<Node>();
+            container.remove_child(n.share());
+            n.queue_free();
+        }
+    }
 }
 
 #[godot_api]
@@ -61,6 +81,7 @@ impl GodotExt for MainGui {
         if Engine::singleton().is_editor_hint() {
         } else {
             self.show_sectors();
+            self.show_fleets();
         }
     }
 
