775daee0eac4a48851a188d186bb5323da28afef
sisso <349345+sisso@users.noreply.github.com>
2023-10-22 09:41:16 -0300

    automatic refresh rust4 on code change

--- scripts/run-godot4.sh
+++ scripts/run-godot4.sh
@@ -3,4 +3,7 @@ set -euo pipefail
 pushd rust/space-godot
 cargo build -p space-godot
 popd
-godot4 godot-space/project.godot
+godot4 godot-space/project.godot &
+pushd rust/space-godot
+cargo watch -x 'build -p space-godot'
+popd
