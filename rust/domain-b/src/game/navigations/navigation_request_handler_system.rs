959d4e6265357c4788a84747e31d97f1acf06abd
sisso <349345+sisso@users.noreply.github.com>
2023-12-10 10:09:42 -0300

    migrate navigation tests

--- rust/domain-b/src/game/navigations/navigation_request_handler_system.rs
+++ rust/domain-b/src/game/navigations/navigation_request_handler_system.rs
@@ -130,117 +130,29 @@ mod test {
         assert!(world.get::<NavRequest>(miner_id).is_none());
     }
 
-    // #[test]
-    // fn test_nav_request_handler_should_create_navigation_from_requests_when_docked() {
-    //     let (world, (asteroid_id, miner_id)) = test_system(NavRequestHandlerSystem, |world| {
-    //         let sector_scenery = setup_sector_scenery(world);
-    //         let (station, asteroid) = setup_station_and_asteroid(world, &sector_scenery);
-    //
-    //         let miner = world
-    //             .spawn_empty()
-    //             .insert(LocationDocked { parent_id: station })
-    //             .insert(NavRequest::MoveToTarget {
-    //                 target_id: asteroid,
-    //             })
-    //             .id();
-    //
-    //         (asteroid, miner)
-    //     });
-    //
-    //     let nav_storage = world.read_component::<Navigation>();
-    //     let nav = nav_storage.get(miner_id).unwrap();
-    //     assert_eq!(
-    //         nav.request,
-    //         NavRequest::MoveToTarget {
-    //             target_id: asteroid_id,
-    //         }
-    //     );
-    // }
-    //
-    // #[test]
-    // fn test_nav_request_handler_should_remove_orbiting_before_move() {
-    //     let (world, (asteroid_id, miner_id)) = test_system(NavRequestHandlerSystem, |world| {
-    //         let sector_scenery = setup_sector_scenery(world);
-    //         let (station_id, asteroid_id) = setup_station_and_asteroid(world, &sector_scenery);
-    //
-    //         let miner = world
-    //             .spawn_empty()
-    //             .insert(LocationSpace {
-    //                 sector_id: sector_scenery.sector_0,
-    //                 pos: P2::X,
-    //             })
-    //             .insert(LocationOrbit {
-    //                 parent_id: asteroid_id,
-    //                 distance: 0.0,
-    //                 start_time: Default::default(),
-    //                 start_angle: 0.0,
-    //                 speed: Speed(0.0),
-    //             })
-    //             .insert(NavRequest::MoveToTarget {
-    //                 target_id: station_id,
-    //             })
-    //             .id();
-    //
-    //         (asteroid_id, miner)
-    //     });
-    //
-    //     assert!(world
-    //         .read_component::<LocationOrbit>()
-    //         .get(miner_id)
-    //         .is_none());
-    //
-    //     assert_eq!(
-    //         world
-    //             .read_component::<Navigation>()
-    //             .get(miner_id)
-    //             .unwrap()
-    //             .request
-    //             .clone(),
-    //         NavRequest::MoveToTarget {
-    //             target_id: asteroid_id,
-    //         }
-    //     );
-    // }
-    //
-    // #[test]
-    // fn test_nav_request_handler_should_navigate_to_orbit() {
-    //     let (world, (asteroid_id, miner_id)) = test_system(NavRequestHandlerSystem, |world| {
-    //         let sector_scenery = setup_sector_scenery(world);
-    //         let asteroid_id = world
-    //             .spawn_empty()
-    //             .insert(LocationSpace {
-    //                 pos: P2::X,
-    //                 sector_id: sector_scenery.sector_1,
-    //             })
-    //             .id();
-    //
-    //         let miner = world
-    //             .spawn_empty()
-    //             .insert(LocationSpace {
-    //                 sector_id: sector_scenery.sector_0,
-    //                 pos: P2::ZERO,
-    //             })
-    //             .insert(NavRequest::OrbitTarget {
-    //                 target_id: asteroid_id,
-    //             })
-    //             .id();
-    //
-    //         (asteroid_id, miner)
-    //     });
-    //
-    //     assert!(world
-    //         .read_component::<LocationOrbit>()
-    //         .get(miner_id)
-    //         .is_none());
-    //
-    //     assert_eq!(
-    //         world
-    //             .read_component::<Navigation>()
-    //             .get(miner_id)
-    //             .map(|i| i.request.clone()),
-    //         Some(NavRequest::OrbitTarget {
-    //             target_id: asteroid_id,
-    //         })
-    //     );
-    // }
+    #[test]
+    fn test_nav_request_handler_should_create_navigation_from_requests_when_docked() {
+        let mut world = World::new();
+        let sector_scenery = setup_sector_scenery(&mut world);
+        let (station_id, asteroid_id) = setup_station_and_asteroid(&mut world, &sector_scenery);
+
+        let miner_id = world
+            .spawn_empty()
+            .insert(LocationDocked {
+                parent_id: station_id,
+            })
+            .insert(NavRequest::MoveToTarget {
+                target_id: asteroid_id,
+            })
+            .id();
+
+        world.run_system_once(system_navigation_request);
+
+        assert_eq!(
+            world.get::<Navigation>(miner_id).unwrap().request,
+            NavRequest::MoveToTarget {
+                target_id: asteroid_id,
+            }
+        );
+    }
 }
